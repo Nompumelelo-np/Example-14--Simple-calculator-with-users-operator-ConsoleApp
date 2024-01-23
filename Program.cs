2b096c0b135e2db54bcaf5e789cfb2d175b35296
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-23 09:23:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_14__Simple_calculator__with_users_operator_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator ");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter second number ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }else if (op == "-") 
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*") 
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if(op == "/") 
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Not an operator");
+            }
+            Console.Read();
+        }     
+    }
+}
